e3546b0a273949beee4cd721d7a1cb355939f653
eduwang <145199866+eduwang@users.noreply.github.com>
2024-07-23 00:28:48 +0900

    minor modification

--- 2_sphere_split.js
+++ 2_sphere_split.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded',() => {
         scene.add(light);
         const ambientLight = new THREE.AmbientLight(0x404040);
         scene.add(ambientLight);
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
         directionalLight.position.set(5, 5, 5).normalize();
         scene.add(directionalLight);
     
